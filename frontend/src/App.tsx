faf612c77b950a3fdf9418272b5042b27c59e1d0
ernilson <ernilson25@gmail.com>
2022-07-16 20:29:00 -0300

    toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
 function App() {
 
   return (
-    <><></><Header /><main>
+    <><></>
+    <ToastContainer />
+    <Header /><main>
       <section id="sales">
         <div className="dsmeta-container">
           <SalesCard />
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../asserts/img/notification-ico.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -10,7 +11,7 @@ function handleClick(id : number){
 
     axios(`${BASE_URL}/sales/${id}/notification2`)
     .then(response => {
-        console.log("Funciona");
+        toast.info("SMS enviado com sucesso");
     });
 }
 
